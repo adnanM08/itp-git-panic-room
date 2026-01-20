fbb32a501e95e01c3b7db0b906e266915154b385
Adnan Muzaferovic <a.muzaferovic@students.htl-leonding.ac.at>
2026-01-20 09:26:22 +0100

    Division by 0 Bug fix & Test "divide_decimal" zu tests hinzugefügt

---
+++ panicRoomLoesung.md
@@ -0,0 +1,9 @@
+# Panic Room
+
+**Ziel:** Das Programm fehlerfrei machen.
+
+### 1 | Tests sind Rot
+**Lösung:**  
+- "return a/0" in Zeile 10 mit "return a/b" ersetzen   
+- Test "divide_decimal" hinzugefügt.
+
--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,7 +7,8 @@ public class Calculator {
 
     public static int divide(int a, int b) {
     // BUG: falscher Divisor -> Division durch 0
-    return a / 0;
+    // LOESUNG: Richtigen Divisor (b) einfügen
+    return a / b;
 }
 
 }
